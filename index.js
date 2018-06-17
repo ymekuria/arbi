68b1ba39cc7845c7fb90e1a533d07bf765c0471f
Yoni <ymekuria@gmail.com>
2018-06-17 15:47:05 -0700

    testing prod routing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const gdaxConnection = require('./sevices/gdaxConnection');
 app.use(bodyParser.json());
 require('./routes/routes')(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (['production'].includes(process.env.NODE_ENV)) {
   // express will serve up production assets
   // like our main.js or main.css file
   app.use(express.static('client/build'));
@@ -15,10 +15,10 @@ if (process.env.NODE_ENV === 'production') {
   // express will serve up the index.hml
   // file if it doesn't understand the route ie react router
   // route
-  const path = require('path');
 
+  const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 const PORT = process.env.PORT || 5000;
