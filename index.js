18616089e3b16d0a71e5f5d66dcc5df8df763017
Yoni <ymekuria@gmail.com>
2018-06-16 22:34:34 -0700

    serving static files in production

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ app.use(bodyParser.json());
 require('./routes/routes')(app);
 
 if (process.env.NODE_ENV === 'production') {
+  // express will serve up production assets
+  // like our main.js or main.css file
   app.use(express.static('client/build'));
 
+  // express will serve up the index.hml
+  // file if it doesn't understand the route ie react router
+  // route
   const path = require('path');
 
   app.get('*', (req, res) => {
