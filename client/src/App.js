3c868a58c06d824e733ca2b5d47eba87d266ec8c
Yoni <ymekuria@gmail.com>
2018-06-17 15:52:52 -0700

    testing prod routing

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a href="/api/test" />
+        <a href="/api/test">TESTAPI</a>
       </div>
     );
   }
