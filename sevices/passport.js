6907e2afd8d9673d7f4cebb70b1bbec800ab82e4
Yoni <ymekuria@gmail.com>
2018-06-23 15:40:25 -0700

    added deserializeUser method

--- sevices/passport.js
+++ sevices/passport.js
@@ -4,3 +4,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
