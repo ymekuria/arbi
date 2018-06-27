4cba543856697a5d6c75b03ec8d414c8b084efc9
Yoni <ymekuria@gmail.com>
2018-06-26 23:05:36 -0700

    added a logout route handler

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,12 @@ module.exports = app => {
 
   app.get('/auth/google/cb', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    // method provided by passport
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
